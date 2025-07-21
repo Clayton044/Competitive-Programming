204c697f5034a0af9b272583f2953d66c8785238
Amir Getnet <143262635+Clayton044@users.noreply.github.com>
2025-07-21 13:50:16 +0300

    Created 'Is Graph Bipartite?' by A2SV Hub

---
+++ 21-Jul-2025/Is Graph Bipartite? 378325.py
@@ -0,0 +1,24 @@
+# Problem: Is Graph Bipartite? - https://leetcode.com/problems/is-graph-bipartite/
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        color = [0] * n  
+
+        for i in range(n):
+            if color[i] != 0:
+                continue
+
+            queue = deque([i])
+            color[i] = 1
+
+            while queue:
+                node = queue.popleft()
+                for neighbor in graph[node]:
+                    if color[neighbor] == 0:
+                        color[neighbor] = -color[node]
+                        queue.append(neighbor)
+                    elif color[neighbor] == color[node]:
+                        return False
+
+        return True
