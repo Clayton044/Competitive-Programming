6e54606ca9b637a668116c0b1f68262149b1782f
Amir Getnet <143262635+Clayton044@users.noreply.github.com>
2025-08-21 21:12:55 +0300

    Created 'N-th Tribonacci Number' by A2SV Hub

---
+++ 21-Aug-2025/N-th Tribonacci Number 387543.py
@@ -0,0 +1,17 @@
+# Problem: N-th Tribonacci Number - https://leetcode.com/problems/n-th-tribonacci-number/description/
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        
+        
+        a, b, c = 0, 1, 1  
+        
+        for i in range(3, n + 1):
+            next_val = a + b + c
+            a, b, c = b, c, next_val  
+        
+        return c        
