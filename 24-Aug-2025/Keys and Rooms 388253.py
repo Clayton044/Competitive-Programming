1ae6b3da125a2a41f4c9528fc9b72d91fe2a8b23
Amir Getnet <143262635+Clayton044@users.noreply.github.com>
2025-08-24 13:50:33 +0300

    Created 'Keys and Rooms' by A2SV Hub

---
+++ 24-Aug-2025/Keys and Rooms 388253.py
@@ -0,0 +1,14 @@
+# Problem: Keys and Rooms - https://leetcode.com/problems/keys-and-rooms/
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+        stack = [0]
+        
+        while stack:
+            room = stack.pop()
+            if room not in visited:
+                visited.add(room)
+                stack.extend(rooms[room])  
+        
+        return len(visited) == len(rooms)
