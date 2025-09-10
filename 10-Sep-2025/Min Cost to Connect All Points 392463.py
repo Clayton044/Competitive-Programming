2e0a999a6092bacda630001596684649e8360f5c
Amir Getnet <143262635+Clayton044@users.noreply.github.com>
2025-09-10 20:50:25 +0300

    Created 'Min Cost to Connect All Points' by A2SV Hub

---
+++ 10-Sep-2025/Min Cost to Connect All Points 392463.py
@@ -0,0 +1,35 @@
+# Problem: Min Cost to Connect All Points - https://leetcode.com/problems/min-cost-to-connect-all-points/
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        parent = list(range(n))
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px != py:
+                parent[px] = py
+        
+        # Calculate all edges (Manhattan distance between points)
+        edges = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                edges.append((dist, i, j))
+        
+        # Sort edges by distance
+        edges.sort()
+        
+        # Kruskal's algorithm
+        total_cost = 0
+        for dist, u, v in edges:
+            if find(u) != find(v):
+                union(u, v)
+                total_cost += dist
+        
+        return total_cost
