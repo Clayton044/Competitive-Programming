00b91489a6b0779b1a4b290cae4a0bed3a6618b8
Amir Getnet <143262635+Clayton044@users.noreply.github.com>
2025-10-10 08:38:10 +0300

    Created 'Partition String ' by A2SV Hub

---
+++ 10-Oct-2025/Partition String  401757.py
@@ -0,0 +1,22 @@
+# Problem: Partition String  - https://leetcode.com/problems/partition-string/description/
+
+class Solution:
+    def partitionString(self, s: str) -> int:
+        seen = set()
+        count = 1
+        for c in s:
+            if c in seen:
+                count += 1
+                seen.clear()
+            seen.add(c)
+        return count
+class Solution:
+    def partitionString(self, s: str) -> int:
+        seen = set()
+        count = 1
+        for c in s:
+            if c in seen:
+                count += 1
+                seen.clear()
+            seen.add(c)
+        return count
